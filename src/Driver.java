f41f911cbb8fa229be0a96ac9deb2acfce04bce2
Dustboonies <dtfeldsine@gmail.com>
2015-08-26 16:27:37 -0700

    Created more methods
    now distinguishes exec files from non exec files

--- src/Driver.java
+++ src/Driver.java
@@ -3,34 +3,33 @@ import java.util.ArrayList;
 
 
 public class Driver {
-	
+	static ArrayList<File> files = new ArrayList<File>();							//Stores all the files in the folder
+	static ArrayList<File> execfiles = new ArrayList<File>();						//Stores executable files from the folder
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ArrayList<File> files = new ArrayList<File>();
-		final File folder = new File("C:/Users/Dustin Feldsine/Desktop");
-		files = listFilesForFolder(folder);
 		
-		for(int a = 0; a < files.size(); a++){
+		final File folder = new File("C:/Users/Dustin Feldsine/Desktop/test");	//Directory where files will be indexed
+		files = listFilesForFolder(folder);										//Updates current files list
+		
+		checkIfExecutable();													//checks for executable files
+		
+		for(int a = 0; a < files.size(); a++){									//Test statement
 			System.out.println(files.get(a));
 		}
 		
-		for(int b = 0; b < files.size(); b++){
-			File file = files.get(b);
-			if(!(file.canExecute()==true)){
-				files.remove(b);
-			}
+		for(int a = 0; a < execfiles.size(); a++){								//Test statement
+			System.out.println(execfiles.get(a));
 		}
 		
-		for(int a = 0; a < files.size(); a++){
-			System.out.println(files.get(a));
-		}
+		
 		
 		
 	}
-	public static ArrayList<File> listFilesForFolder(final File folder) {
+	
+	public static ArrayList<File> listFilesForFolder(final File folder) {		//Retrieves files in the directory
 		ArrayList<File> a = new ArrayList<File>();
 	    for (final File fileEntry : folder.listFiles()) {
 	        if (fileEntry.isDirectory()) {
@@ -42,4 +41,18 @@ public class Driver {
 	    }
 	    return a;
 	}
+	
+	public static void checkIfExecutable(){
+		for(int b = 0; b < files.size(); b++){									//Checks if file is executable
+			File file = files.get(b);
+			String name = file.getName();
+			int length = name.length();
+			String end = (String) (name.subSequence(length-4, length));
+			if(end.equalsIgnoreCase(".exe") || end.equalsIgnoreCase(".msi")){
+				execfiles.add(file);
+			}
+		}
+	}
+	
+	
 }
